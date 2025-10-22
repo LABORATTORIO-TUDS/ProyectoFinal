74e0e657766d07ebf0f7a7421a1d0e4090835827
victormarianopelaez-glitch <victormarianopelaez@gmail.com>
2025-10-22 20:12:39 -0300

    Agregado jconector y conexion.java

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -3,11 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
- * @author elias
+ * @author victor
  */
 public class Conexion {
+       private static Connection c = null;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/gp13_cinemacentro";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "";
+
+    public static Connection conectar() {
+        if (c == null) {
+            try {
+                c = DriverManager.getConnection(URL, USUARIO, CLAVE);
+                System.out.println("✅ Conexion exitosa a la base de datos.");
+            } catch (SQLException e) {
+                System.out.println("❌ Error en la conexion: " + e.getMessage());
+                return null;
+            }
+        }
+         return c;
+    }
+   
+
+    public static void cerrarConexion() {
+        try {
+            if (c != null) {
+                c.close();
+                System.out.println("Conexion cerrada");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al cerrar la conexion: "+ e);
+                  
+        }
+    }
     
 }
